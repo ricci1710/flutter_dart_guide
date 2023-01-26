f17ecf114c7fecbc9ff5d39fca8da8939101ba6e
ricci1710 <ricci1710@gmail.com>
2023-01-26 18:52:46 +0100

    28. Building a Widget Tree

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text('Hello!'),
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+            title: const Text('My first App'),
+        ),
+        body: const Text('This is my default text!'),
+      ),
     );
   }
 }
