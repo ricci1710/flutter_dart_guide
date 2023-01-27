d7d05961654c2ec7168122c316a56398cfb28c93
ricci1710 <ricci1710@gmail.com>
2023-01-27 11:02:51 +0100

    28. Building a Widget Tree

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,36 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void answerQuestion(int value) {
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.white,
         appBar: AppBar(
-            title: const Text('My first App'),
+          title: const Text('My first App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: () => answerQuestion(1),
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => answerQuestion(2),
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => answerQuestion(3),
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
