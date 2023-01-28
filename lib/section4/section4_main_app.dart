febb8550eaae543b95e47901dee57e170eb1eba0
ricci1710 <ricci1710@gmail.com>
2023-01-28 13:36:09 +0100

    section4: 78. Styling a Container

--- lib/section4/section4_main_app.dart
+++ lib/section4/section4_main_app.dart
@@ -45,6 +45,17 @@ class Section4HomePage extends StatelessWidget {
                         child: Row(
                           children: [
                             Container(
+                              margin: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 15,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(10),
                               child: Text(
                                 tx.amount.toString(),
                               ),
