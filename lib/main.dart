3113407cace154ffee9ea41b1612d40d4889c01f
Rico Kunz <ricci1710@gmail.com>
2023-01-28 10:10:42 +0100

    section 2: Run on IOS Physical Device

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dart_guide/section2/exercise/flutter_assignment_main.dart';
+import 'package:flutter_dart_guide/section2/section2_main_app.dart';
 
-void main() => runApp(const FlutterAssignmentApp());
+void main() => runApp(const Section2MainApp());
