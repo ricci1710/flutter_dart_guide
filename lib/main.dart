4f3a650ebb7e38563b927781b31a43c351bf00c8
Rico Kunz <rico.kunz@leutek.de>
2023-02-02 10:04:32 +0100

    section 5: 124. Controlling the Device Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dart_guide/section4/section4_main_app.dart';
 
-void main() => runApp(const Section4MainApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const Section4MainApp());
+}
