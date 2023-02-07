27539ea204ecc515a57d36b080c58d7a070a06a6
Rico Kunz <rico.kunz@leutek.de>
2023-02-07 10:02:09 +0100

    section7: 165. Using Named Routes & Passing Data With Named Routes

--- lib/section7/widgets/category_meals_screen.dart
+++ lib/section7/widgets/category_meals_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  // final String categoryId;
-  // final String categoryTitle;
-
   const CategoryMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    // final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('categoryTitle'),
+        title: Text(categoryTitle!),
       ),
       body: const Center(
         child: Text(
