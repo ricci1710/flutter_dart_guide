cf00c86a302a26d6da63748d18191ec6137b2268
Rico Kunz <rico.kunz@leutek.de>
2023-02-07 08:12:25 +0100

    section7: 164. Passing Data via the Constructor

--- lib/section7/widgets/categories_screen.dart
+++ lib/section7/widgets/categories_screen.dart
@@ -26,6 +26,7 @@ class CategoriesScreen extends StatelessWidget {
         children: dummyCategories
             .map(
               (catData) => CategoryItem(
+                catData.id,
                 catData.title,
                 catData.color,
               ),
---
+++ lib/section7/widgets/category_meals_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  const CategoryMealsScreen(this.categoryId, this.categoryTitle, {super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: const Center(
+        child: Text(
+          'The Recipes For The Category!',
+        ),
+      ),
+    );
+  }
+}
