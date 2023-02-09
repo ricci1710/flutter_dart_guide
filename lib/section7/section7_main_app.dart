4d29570a14a468b707d73d73b0f02814220d8123
Rico Kunz <rico.kunz@leutek.de>
2023-02-09 10:17:14 +0100

    section7: 174. Adding a TabBar to the AppBar

--- lib/section7/section7_main_app.dart
+++ lib/section7/section7_main_app.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 class Section7MainApp extends StatelessWidget {
   const Section7MainApp({super.key});
@@ -36,7 +37,7 @@ class Section7MainApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => const CategoriesScreen(),
+        '/': (ctx) => const TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
       },
