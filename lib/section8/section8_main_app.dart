4b0d2483e7442423dfbc66ff3fdb53dc407fbbed
Rico Kunz <rico.kunz@leutek.de>
2023-02-27 17:23:30 +0100

    section7: 189. Working on the "Products" Grid & Item Widgets

--- lib/section8/section8_main_app.dart
+++ lib/section8/section8_main_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 class Section8MainApp extends StatelessWidget {
   const Section8MainApp({Key? key}) : super(key: key);
 
@@ -9,23 +11,7 @@ class Section8MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'MyShop',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-      ),
-      body: const Center(
-        child: Text('Let\'s build a shop!'),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
