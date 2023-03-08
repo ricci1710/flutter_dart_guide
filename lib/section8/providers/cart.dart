1188537867bd07ae0bfb5b11b7c92835662288fa
Rico Kunz <rico.kunz@leutek.de>
2023-03-08 13:36:29 +0100

    section7: 202. Adding Shopping Card Data

---
+++ lib/section8/providers/cart.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({required this.id, required this.title, required this.price, required this.quantity});
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      // change quantity...
+      _items.update(
+          productId,
+          (existingCardItem) => CartItem(
+                id: existingCardItem.id,
+                title: existingCardItem.title,
+                price: existingCardItem.price,
+                quantity: existingCardItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
